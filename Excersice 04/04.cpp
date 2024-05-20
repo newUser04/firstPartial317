759f0ec660b0df8383817d2685a035503ddeef6e
newUser04 <147574761+newUser04@users.noreply.github.com>
2024-05-20 07:22:16 +0100

    Update 04.cpp

--- Excersice 04/04.cpp
+++ Excersice 04/04.cpp
@@ -6,29 +6,31 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-  int n = 8, a[n][n], b[n], c[n], c_aux = 1;
+  int n = 8;
+  int a[n][n], b[n], c[n], c_aux = 1;
   for(int i = 1; i <= n; i++)
   {
     b[i - 1] = i;
+    c[i - 1] = 0;
     for(int j = 1; j <= n; j++)
     {
-      a[i - 1][j -1 ] = c_aux;
+      a[i - 1][j - 1] = c_aux;
       c_aux++;
     }
   }
 
   printf("Vector n:\n\t");
-  for(int i = 0; i < 8; i++)
+  for(int i = 0; i < n; i++)
   {
     printf("| %d ", b[i]);
   }
   printf("|\n");
 
   printf("Matriz n*n:\n");
-  for(int i = 0; i < 8; i++)
+  for(int i = 0; i < n; i++)
   {
     printf("\t");
-    for(int j = 0; j < 8; j++)
+    for(int j = 0; j < n; j++)
     {
       printf("| %d", a[i][j]);
     }
@@ -36,12 +38,12 @@ int main(int argc, char** argv)
   }
 
   #pragma omp parallel for
-  for(int i = 0; i < n; i++)
+  for(int k = 0; k < n; k++)
   {
-    for(int j = 0; j < n; j++)
+    for(int l = 0; l < n; l++)
     {
-      printf("Process: %d || i: %d || j: %d\n", omp_get_thread_num(), i, j);
-      c[i] += a[i][j] * b[j];
+      printf("Process: %d || i: %d || j: %d\n", omp_get_thread_num(), k, l);
+      c[k] += a[k][l] * b[l];
     }
   }
   printf("----------------------\n");
